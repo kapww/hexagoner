ae4650c31dfe911a1d0c207e5aab7243554b918a
Kate West-Walker <k.west.walker@gmail.com>
2013-02-13 21:02:50 +1300

    Added blog app, and fixed config issues

---
+++ katesite/blog/views.py
@@ -0,0 +1,6 @@
+# from blog.models import Post
+
+# def main(request):
+#    posts = Post.objects.all().order_by("-created")
+
+#    return render_to_response("list.html", dict(posts=posts, user=request.user))
